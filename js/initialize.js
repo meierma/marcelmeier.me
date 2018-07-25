0b89be71901f356db5d5818aed254cd17f4f6087
Marcel Meier <meierma@uni-bremen.de>
2018-07-25 19:46:10 +0200

    Implemented particlejs

---
+++ js/initialize.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+
+    $('.parallax').parallax();
+
+    particlesJS.load('particles-js-white', '/config/particlesjs-config-white.json', function(){});
+
+    particlesJS.load('particles-js-black', '/config/particlesjs-config-black.json', function(){});
+});
