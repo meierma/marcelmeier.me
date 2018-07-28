eedcea123509b5385719eed9ca2c66fc36dc3e1c
Marcel Meier <meierma@uni-bremen.de>
2018-07-28 21:20:28 +0200

    Implement Rellax as Parallax

--- js/initialize.js
+++ js/initialize.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 
-    $('.parallax').parallax();
-
+    var rellax = new Rellax('.parallax');
+    
     particlesJS.load('particles-js-white', '/config/particlesjs-config-white.json', function(){});
 
     particlesJS.load('particles-js-black', '/config/particlesjs-config-black.json', function(){});
