062f73d4d9e5981c1acb4eeafe448ded1cd37a70
Marcel Meier <meierma@uni-bremen.de>
2020-01-11 16:05:11 +0100

    Inital redesign

--- js/initialize.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-
-    var rellax = new Rellax('.parallax');
-    
-    particlesJS.load('particles-js-white', '/config/particlesjs-config-white.json', function(){});
-
-    particlesJS.load('particles-js-black', '/config/particlesjs-config-black.json', function(){});
-});
